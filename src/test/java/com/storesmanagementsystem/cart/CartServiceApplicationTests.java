e87bb0bf8244ae453679d768a83d5c0e860aefc3
chanduv33 <chandu.teja.0557@gmail.com>
2025-02-03 00:29:38 +0530

    props added

--- src/test/java/com/storesmanagementsystem/cart/CartServiceApplicationTests.java
+++ src/test/java/com/storesmanagementsystem/cart/CartServiceApplicationTests.java
@@ -3,12 +3,14 @@ package com.storesmanagementsystem.cart;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CartServiceApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+	@Test
+	void contextLoads() {
+	}
 
 }
